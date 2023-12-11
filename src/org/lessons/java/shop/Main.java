3d6051792dc957ca032f6a33f7712b77d9dd2c2e
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-12-11 14:24:33 +0100

    refactor product

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,53 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Importo lo scanner
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Chiedo all'utente il nome del nuovo prodotto
+            System.out.println("Insert the product's name: ");
+            String name = scanner.nextLine();
+
+
+            // Chiedo all'utente la descrizione del nuovo prodotto
+            System.out.println("Insert the product's description: ");
+            String description = scanner.nextLine();
+
+            // Chiedo all'utente il prezzo del nuovo prodotto
+            System.out.println("Insert the product's price: ");
+            BigDecimal price = BigDecimal.valueOf(Double.parseDouble(scanner.nextLine()));
+
+            // Chiedo all'utente l'iva
+            System.out.println("Insert the current VAT: ");
+            BigDecimal vat = BigDecimal.valueOf(Double.parseDouble(scanner.nextLine()));
+
+            // Chiedo all'utente la categoria del prodotto
+            System.out.println("Insert the product's category: ");
+            String categoryName = scanner.nextLine();
+
+            // Chiedo all'utente la descrizione della categoria
+            System.out.println("Insert the category's description: ");
+            String categoryDescription = scanner.nextLine();
+
+            // Creo una nuova categoria con le informazioni date
+            Category category = new Category(categoryName, categoryDescription);
+
+            // Creo un nuovo prodotto con le informazioni date
+            Product product = new Product(name, description, price, vat);
+            product.setCategory(category);
+
+            // Stampo a video
+            System.out.println(product.getProductInfo());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Chiudo lo scanner
+            scanner.close();
+        }
+    }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,2 +1,141 @@
-package org.lessons.java.shop;public class Product {
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Product {
+    // ATTRIBUTI
+    private int code;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private BigDecimal vat;
+
+    // Categoria
+    private Category category;
+
+    // COSTRUTTORI
+
+    public Product(String name, String description, BigDecimal price, BigDecimal vat) throws IllegalArgumentException{
+        validateName(name);
+        validatePrice(price);
+        validateVat(vat);
+        codeGenerator();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
+
+
+    // GETTER E SETTER
+
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws IllegalArgumentException{
+        validateName(name); // prima testo il valore che viene da fuori: se non valido si interrompe con eccezione
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getVat() {
+        return vat;
+    }
+
+    public void setVat(BigDecimal vat) {
+        this.vat = vat;
+    }
+
+
+    // METODI
+
+
+    // Metodo che mi genera un codice casuale
+    private void codeGenerator () {
+        Random randomCode = new Random();
+        this.code = randomCode.nextInt(1, 10001);
+    }
+
+    // Metodo che mi calcola il prezzo con l'iva
+    public BigDecimal getVatPrice() {
+        BigDecimal vatPrice = price.multiply(vat);
+        return price.add(vatPrice).setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    // Metodo che mi stampa codice-nome dell'oggetto
+    public String getFullName() {
+        return "<-----Product----->" + '\n' +
+                getCode() + " - " + getName() + '\n';
+    }
+
+    // Metodo che mi stampa le informazioni dell'oggetto
+    public String getProductInfo() {
+        return getFullName() +
+
+                " " + '\n' +
+                "<-----Product Info----->" + '\n' +
+                "Name: " + getName() + '\n' +
+                "Description: " + getDescription() + '\n' +
+                "Price without VAT: " + getPrice() + " €" + '\n' +
+                "Price + VAT: " + getVatPrice() + " €" + '\n' +
+                "Product Code: " + getCode() + '\n' +
+
+                " " + '\n' +
+                "<-----Category----->" + '\n' +
+                "Category name: " + category.getName() + '\n' +
+                "Category description: " + category.getDescription();
+    }
+
+    // metodi di validazione
+    private void validateName(String name) throws IllegalArgumentException{
+        if(name == null || name.isEmpty()){
+            // name non è valido
+            throw new IllegalArgumentException("name empty");
+        }
+    }
+
+    private void validatePrice(BigDecimal price) throws IllegalArgumentException{
+        if(price == null || price.compareTo(new BigDecimal(0)) < 0){
+            // prezzo negativo
+            throw new IllegalArgumentException("price negative " + price);
+        }
+    }
+
+    private void validateVat(BigDecimal vat) throws IllegalArgumentException{
+        if(vat == null || vat.compareTo(new BigDecimal(0))< 0){
+            throw new IllegalArgumentException("vat negative " + vat);
+        }
+    }
+
 }
